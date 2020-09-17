aa8c26039d7f7678e54533ceb7be2d2aaff737f8
unknown <prinjalboruah@gmail.com>
2020-09-17 12:23:35 +0530

    added select,date,time

--- pager/models.py
+++ pager/models.py
@@ -5,6 +5,8 @@ class SpeechData(models.Model):
     manual_id = models.IntegerField(unique=True, null=True, blank=True)
     text = models.TextField()
     audioFile = models.FileField(upload_to='audio/', null=True, blank=True)
+    date = models.CharField(max_length=100)
+    time = models.CharField(max_length=100)
 
     def __str__(self):
         return self.text
--- pager/views.py
+++ pager/views.py
@@ -4,6 +4,8 @@ from gtts import gTTS
 import time
 from django.http import HttpResponse
 import requests
+from datetime import datetime
+from pytz import timezone
 
 def indexView(request):
     x = requests.get('http://106.51.15.203/api/api.php?action=LIST&type=PAGE')
@@ -23,6 +25,12 @@ def saveTextAudio(request):
     received_text = request.GET.get("text", None)
     received_lang = request.GET.get("language", None)
 
+    now_utc = datetime.now(timezone('UTC'))
+    now_asia = now_utc.astimezone(timezone('Asia/Kolkata'))
+
+    date_now = now_asia.strftime("%d/%m/%Y")
+    time_now = now_asia.strftime("%I:%M:%S %p")
+
     if received_text:
         if received_lang == "en-US":
             language = 'en'
@@ -34,6 +42,6 @@ def saveTextAudio(request):
         audio_save_location_django = "audio/"+current_time+".mp3"
         myobj = gTTS(text=received_text, lang=language, slow=False)
         myobj.save(audio_save_location)
-        savetext = SpeechData(text=received_text, manual_id=current_time, audioFile=audio_save_location_django, lang=language)
+        savetext = SpeechData(text=received_text, manual_id=current_time, audioFile=audio_save_location_django, lang=language, date = date_now, time = time_now)
         savetext.save()
         return HttpResponse("Saved successfully")
