bda5302b208e17d9e2fd9ba0b7517ffb3f1f1ecc
unknown <prinjalboruah@gmail.com>
2020-09-23 15:25:36 +0530

    bug fix 5

--- bnaPager/wsgi.py
+++ bnaPager/wsgi.py
@@ -17,12 +17,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bnaPager.settings')
 
 application = get_wsgi_application()
 
-##for heroku
-
-# import os
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bnaPager.settings')
-# from django.core.wsgi import get_wsgi_application
-# from whitenoise.django import DjangoWhiteNoise
-
-# application = get_wsgi_application()
-# application = DjangoWhiteNoise(application)
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
